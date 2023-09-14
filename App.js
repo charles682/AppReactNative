83b74cefe753e788216d2207938b29e10a299f21
Charles Melo <melocharles220@gmail.com>
2023-09-14 19:00:26 -0300

    Update home add button and slider

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native'
+import Slider from '@react-native-community/slider'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>sujeito Programador!</Text>
-      <StatusBar style="auto" />
+     <Image
+       source={require("./src/assets/logoo.png")}
+       style={styles.logo}
+       
+       />
+       
+     <Text style={styles.title}>20 caractres </Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{height: 50}}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor="#ff0000"
+          minimumTrackTintColor="#000"
+          />
+      </View>
+
+      <TouchableOpacity>
+        <Text>Gerar Senha</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F3F3FF",
+    backgroundColor: 'rgba(243, 243, 255, 1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    marginBottom: 60
+  },
+  area:{
+    marginTop:14,
+    marginBottom:14,
+    width:"80%",
+    backgroundColor: "#fff",
+    borderRadius:8,
+    padding:8,
+  }
 });
